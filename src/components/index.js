6b8474d39ae48211b3785d4dd52a21f67a122408
Festivemena <88979259+Festivemena@users.noreply.github.com>
2023-05-05 21:51:08 +0100

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import ChannelCard from './ChannelCard'
+import ChannelDetail from './ChannelDetail'
+import Feed from './Feed'
+import Loader from './Loader'
+import Navbar from './Navbar'
+import SearchBar from './SearchBar'
+import SearchFeed from './SearchFeed'
+import Sidebar from './Sidebar'
+import VideoCard from './VideoCard'
+import VideoDetail from './VideoDetail'
+import Videos from './Videos'
+
+export {ChannelCard, ChannelDetail, Feed, Loader, Navbar, SearchBar, Sidebar, SearchFeed, VideoCard, VideoDetail, Videos}
